1363c6cfd866fd785c97eeedc8e69725a7f35e3b
iamsurajx <surajc8800@gmail.com>
2024-03-24 08:47:15 +0530

    ABC commit again

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function sayMyName() {
 }
 
 sayMyName(); // Call the function
+
+()=>(console.log('Arrow Function'))(); // Arrow Function Example
+
+// sum of two numbers
+const add = (a, b) => a + b;
+
+console.log(add(2, 3)); // Output: 5
